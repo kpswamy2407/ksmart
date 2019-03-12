d1ab1615f7cace51c74eaf35bccc863d0e37c8dd
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-03-12 13:57:49 +0530

    refactored

--- routes/configure.post.js
+++ routes/configure.post.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports=(req,res,next)=>{
 	const ConfigHelper=require('./../helper/ConfigHelper.js');
-	var config=new ConfigHelper();
+	var config=new ConfigHelper(req.params.domain);
 	try{
 		config.setBasePath(process.env.DOMAINS_XML_PATH);
-		config.mkCompanyDir(req.params.domain);
+		config.createCompany();
 		config.setBasePath(process.env.DOMAINS_XML_PATH+'/'+req.params.domain);
 		config.save(req.params.config,req.body.toString());
 		res.end();
