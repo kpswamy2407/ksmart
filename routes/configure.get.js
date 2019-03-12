6d7e1c277e3dc3c3a16482a379219cdcfedc36e9
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-03-12 14:09:58 +0530

    code has been updated to accept ConfigHelper

--- routes/configure.get.js
+++ routes/configure.get.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports=(req,res,next)=>{
 	const ConfigHelper=require('./../helper/ConfigHelper.js');
-	var config=new ConfigHelper();
+	var config=new ConfigHelper(req.params.domain);
 	try{
 		config.setBasePath(process.env.DOMAINS_XML_PATH);
-		var file=config.configPath(req.params.domain,req.params.config);
+		var file=config.configPath(req.params.config);
 		res.sendFile(file);
 	}
 	catch(e){
