fd757d0be94fb092581d3f81910c23d3cf45956a
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-03-12 18:32:16 +0530

    /:domain/xdistdeviceregistration is being implemented

--- routes/esbRouter.js
+++ routes/esbRouter.js
@@ -1,6 +1,19 @@
 const express=require('express');
 var router=express.Router();
-router.post('/:domain/xdistdeviceregistration', function(req, res, next) {
+const bodyParser=require('body-parser');
+var xmlBodyParser=require('express-xml-bodyparser');
+const HttpError=require('./../error/HttpError.js');
 
+router.post('/:domain/xdistdeviceregistration',xmlBodyParser({
+	trim: false, explicitArray: false,
+}),function(req, res, next) {
+	var accept=[
+		'application/xml',
+		'text/xml',
+		'application/json',
+	];
+	if( !accept.includes(req.get('Content-Type')) )
+		throw new HttpError(404,'ERR-xx-xxxx','Invalid Content-Type request header.');
+	res.end();
 });
 module.exports = router;
