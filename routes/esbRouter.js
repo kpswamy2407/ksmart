95686943d28a1d2bc12c24a85436612005f68971
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-03-13 14:04:32 +0530

    integration of xDistDeviceregister module is being done.

--- routes/esbRouter.js
+++ routes/esbRouter.js
@@ -3,7 +3,9 @@ var router=express.Router();
 const bodyParser=require('body-parser');
 var xmlBodyParser=require('express-xml-bodyparser');
 const HttpError=require('./../error/HttpError.js');
+require('dotenv').config();
 
+const xDistDeviceRegister=require('./../helper/xDistDeviceRegister');
 router.post('/:domain/xdistdeviceregistration',xmlBodyParser({
 	trim: false, explicitArray: false,
 }),function(req, res, next) {
@@ -14,6 +16,17 @@ router.post('/:domain/xdistdeviceregistration',xmlBodyParser({
 	];
 	if( !accept.includes(req.get('Content-Type')) )
 		throw new HttpError(404,'ERR-xx-xxxx','Invalid Content-Type request header.');
-	res.end();
+	var xddr=new xDistDeviceRegister(req.params.domain,req.body);
+	try{
+		xddr.setBasePath(process.env.DOMAINS_XML_PATH);
+		xddr.register().then(()=>{
+			res.status(200).end();
+		}).catch(e=>{
+			next(new HttpError(500,'ERR-xx-xxxx',e.message));
+		});
+	}
+	catch(e){
+		throw new HttpError(500,'ERR-xx-xxxx',e.message);
+	}
 });
 module.exports = router;
