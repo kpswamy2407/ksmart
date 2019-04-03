7c2910b12ddc1f625d76189dcf65aa911824c492
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-04-03 15:27:52 +0530

    logger module integrated.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,8 +17,14 @@ app.set('view engine', 'jade');
 app.use('/run-query/',require('./routes/run-query'));
 const AuthFilter=require('./filter/AuthFilter.js');
 app.use(AuthFilter);
-
-app.use(logger('dev'));
+/** LOGGER STARTS **/
+const rfs=require('rotating-file-stream');
+var accessLogStream=rfs('access.log',{
+	interval:'1d',
+	path:path.join(__dirname,'logs'),
+});
+app.use(morgan('combined',{stream:accessLogStream}));
+/** LOGGER ENDS **/
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
