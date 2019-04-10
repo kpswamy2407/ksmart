58a091742adfd4443bb89dde998717ace95d1ab7
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-04-10 13:32:48 +0530

    spaces btwn = symbol has been formatted.

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,23 @@
-var express = require("express");
-var routes = express.Router();
-const Sequelize = require('sequelize');
-const AuthHelper = require('../helper/authHelper');
-const authHelper = new AuthHelper();
-const IoHelper = require('../helper/ioHelper');
-const RedisHelper = require('../helper/redisHelper');
-const ioHelper = new IoHelper();
-const redisHelper = new RedisHelper();
+var express=require("express");
+var routes=express.Router();
+const Sequelize=require('sequelize');
+const AuthHelper=require('../helper/authHelper');
+const authHelper=new AuthHelper();
+const IoHelper=require('../helper/ioHelper');
+const RedisHelper=require('../helper/redisHelper');
+const ioHelper=new IoHelper();
+const redisHelper=new RedisHelper();
 require('dotenv').config();
-const HttpError = require('../error/HttpError');
+const HttpError=require('../error/HttpError');
 routes.get('/:domain/Authentication', function (req, res, next) {
 	try {
 		authHelper.getMd5(authHelper.getRandomString()).then(authToken => {
 			authHelper.getMd5(authHelper.getRandomString()).then(salt => {
 				redisHelper.setValue(authToken + '_' + req.ip, salt, process.env.REDIS_MIN_TTL);
-				var data = {};
-				data.authtoken = authToken;
-				data.dynasalt = salt;
-				var isXMLResponse = (req.query.format == 'extjson') ? false : true;
+				var data={};
+				data.authtoken=authToken;
+				data.dynasalt=salt;
+				var isXMLResponse=(req.query.format == 'extjson') ? false : true;
 				ioHelper.getSuccessResponse({
 					'collections': {
 						'authentication': data,
@@ -31,12 +31,12 @@ routes.get('/:domain/Authentication', function (req, res, next) {
 	}
 });
 routes.post('/:domain/Authentication', (req, res, next) => {
-	const ConfigHelper = require('../helper/ConfigHelper')
-	var configHelper = new ConfigHelper(req.params.domain);
+	const ConfigHelper=require('../helper/ConfigHelper')
+	var configHelper=new ConfigHelper(req.params.domain);
 	configHelper.setBasePath(process.env.DOMAINS_XML_PATH);
 	try {
-		dbConfig = configHelper.load('dms');
-		const sequelize = new Sequelize(dbConfig.getKey("centralmastermysqldatabase"), null, null, {
+		dbConfig=configHelper.load('dms');
+		const sequelize=new Sequelize(dbConfig.getKey("centralmastermysqldatabase"), null, null, {
 			dialect: 'mysql',
 			replication: {
 				write: {
@@ -54,24 +54,24 @@ routes.post('/:domain/Authentication', (req, res, next) => {
 			},
 			logging: req.app.get('__fnxtlogger__'),
 		});
-		var authToken = req.query.auth_token;
-		var domain = req.params.domain;
-		var isXMLResponse = (req.query.format == 'extjson') ? false : true;
-		var response = {};
+		var authToken=req.query.auth_token;
+		var domain=req.params.domain;
+		var isXMLResponse=(req.query.format == 'extjson') ? false : true;
+		var response={};
 		switch (req.get('content-type')) {
 			case 'application/xml':
 			case 'text/xml':
 			case 'application/json':
 			case 'application/json; charset=UTF-8':
-				const input = req.body.collections.authentication;
-				const authConfig = configHelper.load('auth');
+				const input=req.body.collections.authentication;
+				const authConfig=configHelper.load('auth');
 				switch (input.ldapentitytype) {
 					case 'userprofile':
 						redisHelper.getValue(req.query.auth_token + "_" + req.ip).then(dynasaltValue => {
 							if (!dynasaltValue) {
 								throw new HttpError(404, "ERR-AU-0005", "Property info missing")
 							}
-							var query = authConfig.getKey("userprofilesql").replace("$whereCondition", "user_name=:user_name");
+							var query=authConfig.getKey("userprofilesql").replace("$whereCondition", "user_name=:user_name");
 							return sequelize.query(query, {
 								replacements: {
 									user_name: input.user_name
@@ -86,13 +86,13 @@ routes.post('/:domain/Authentication', (req, res, next) => {
 										throw new HttpError(401, 'ERR-AU-0001', 'Inactive User')
 									}
 									redisHelper.setValue(authToken, authToken, process.env.REDIS_MAX_TTL)
-									response.authusername = input.user_name;
-									response.authtoken = authToken;
-									response.aclrole = user.role;
-									response.isactive = user.isactive;
-									response.status = true;
+									response.authusername=input.user_name;
+									response.authtoken=authToken;
+									response.aclrole=user.role;
+									response.isactive=user.isactive;
+									response.status=true;
 									authHelper.getMd5(authHelper.getRandomString() + response.authusername + req.ip + authToken + domain).then(apiKey => {
-										response.apikey = apiKey;
+										response.apikey=apiKey;
 										redisHelper.setValue(authToken + "_apikey", apiKey, process.env.REDIS_MAX_TTL);
 										ioHelper.getSuccessResponse({
 											'collections': response,
@@ -111,13 +111,13 @@ routes.post('/:domain/Authentication', (req, res, next) => {
 							if (!validToken) {
 								throw new HttpError(404, "ERR-AU-0005", "Property info missing")
 							}
-							var queryString = "";
+							var queryString="";
 							Object.entries(input).forEach(([key, value]) => {
 								if (key != 'ldapentitytype') {
-									queryString = queryString + key + "='" + value + "' and ";
+									queryString=queryString + key + "='" + value + "' and ";
 								}
 							});
-							queryString = queryString.trim(" ").slice(0, -3)
+							queryString=queryString.trim(" ").slice(0, -3)
 							sequelize.query(authConfig.getKey("distributorprofilesql").replace("$whereCondition", queryString), {
 								type: sequelize.QueryTypes.SELECT
 							}).spread(queryRes => {
@@ -125,13 +125,13 @@ routes.post('/:domain/Authentication', (req, res, next) => {
 									throw new HttpError(401, 'ERR-AU-0001', 'Inactive User')
 								}
 								redisHelper.setValue(authToken, authToken, process.env.REDIS_MAX_TTL)
-								response.authusername = input.distributorid + input.clientid + input.cdkey + input.deviceuniquekey;
-								response.authtoken = authToken;
-								response.aclrole = "distributor";
-								response.isactive = queryRes.isactive;
-								response.status = true;
+								response.authusername=input.distributorid + input.clientid + input.cdkey + input.deviceuniquekey;
+								response.authtoken=authToken;
+								response.aclrole="distributor";
+								response.isactive=queryRes.isactive;
+								response.status=true;
 								authHelper.getMd5(authHelper.getRandomString() + response.authusername + req.ip + authToken + domain).then(apiKey => {
-									response.apikey = apiKey;
+									response.apikey=apiKey;
 									redisHelper.setValue(authToken + "_apikey", apiKey, process.env.REDIS_MAX_TTL);
 									ioHelper.getSuccessResponse({
 										'collections': response,
@@ -146,13 +146,13 @@ routes.post('/:domain/Authentication', (req, res, next) => {
 							if (!validToken) {
 								throw new HttpError(404, "ERR-AU-0005", "Property info missing")
 							}
-							var queryString = "";
+							var queryString="";
 							Object.entries(input).forEach(([key, value]) => {
 								if (key != 'ldapentitytype') {
-									queryString = queryString + key + "='" + value + "' and ";
+									queryString=queryString + key + "='" + value + "' and ";
 								}
 							});
-							queryString = queryString.trim(" ").slice(0, -3)
+							queryString=queryString.trim(" ").slice(0, -3)
 							sequelize.query(authConfig.getKey("salesmanprofilesql").replace("$whereCondition", queryString), {
 								type: sequelize.QueryTypes.SELECT
 							}).spread(queryRes => {
@@ -160,13 +160,13 @@ routes.post('/:domain/Authentication', (req, res, next) => {
 									throw new HttpError(401, 'ERR-AU-0001', 'Inactive User')
 								}
 								redisHelper.setValue(authToken, authToken, process.env.REDIS_MAX_TTL)
-								response.authusername = input.salesmanid + input.clientid + input.cdkey + input.deviceuniquekey;
-								response.authtoken = authToken;
-								response.aclrole = "salesman";
-								response.isactive = queryRes.isactive;
-								response.status = true;
+								response.authusername=input.salesmanid + input.clientid + input.cdkey + input.deviceuniquekey;
+								response.authtoken=authToken;
+								response.aclrole="salesman";
+								response.isactive=queryRes.isactive;
+								response.status=true;
 								authHelper.getMd5(authHelper.getRandomString() + response.authusername + req.ip + authToken + domain).then(apiKey => {
-									response.apikey = apiKey;
+									response.apikey=apiKey;
 									redisHelper.setValue(authToken + "_apikey", apiKey, process.env.REDIS_MAX_TTL);
 									ioHelper.getSuccessResponse({
 										'collections': response,
@@ -200,4 +200,4 @@ routes.get('/test', (req, res, next) => {
 		'status': 'Node application started'
 	});
 })
-module.exports = routes;
+module.exports=routes;
