81b0da281ef6238a2b5a8fe11d595480b1a32f52
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-04-11 14:06:50 +0530

    code restructured to fix issue and bring everything under try block.

--- helper/downloadHelper.js
+++ helper/downloadHelper.js
@@ -13,33 +13,38 @@ DownloadHelper.getResult = function(req,res,next) {
     try{
         var query=downloadConfig.getKey(req.params.key);   
     }catch(e){
-        next(new HttpError(404,'FN-XX-xxxx','Service "'+req.params.key+'" is not available.'))
+        throw new HttpError(404,'FN-XX-xxxx','Service "'+req.params.key+'" is not available.');
     }
-    var isXMLResponse = (req.query.format == 'extjson') ? false : true;
-    var queryMetaKeys=query.match(/#\$[a-zA-Z]{1,}#/gm);
-    for (var i = queryMetaKeys.length - 1; i >= 0; i--) {
-        var qkey=queryMetaKeys[i].replace(/#/g,"").replace("$","");
-        query=query.replace(queryMetaKeys[i],req.query[qkey]);
-    }
-    configHelper.setLoggerFn(req.app.get('__fnxtlogger__'));
-    const db=configHelper.getDb();
-    var queryResult=db.query(query,{type: db.QueryTypes.SELECT});
-    queryResult.catch(()=>{
-        next(new HttpError(500,'ERR-CON-0021','Unknown error occured'));
-    }).finally(()=>{
-        db.close();
-    });
-    if(/[a-zA-Z]{1,}count$/.test(req.params.key)){
-        queryResult.spread(count=>{
-            ioHelper.getSuccessResponse({collections:{response:count,rowcount:1}},isXMLResponse,res)
-        }); 
-    }
-    else{
-        queryResult.then(result=>{
-            ioHelper.getSuccessResponse({collections:{response:result,rowcount:result.length}},isXMLResponse,res)
+    try{
+        var isXMLResponse = (req.query.format == 'extjson') ? false : true;
+        var queryMetaKeys=query.match(/#\$[a-zA-Z]{1,}#/gm);
+        if(queryMetaKeys!==null){
+            for (var i = queryMetaKeys.length - 1; i >= 0; i--) {
+                var qkey=queryMetaKeys[i].replace(/#/g,"").replace("$","");
+                query=query.replace(queryMetaKeys[i],req.query[qkey]);
+            }
+        }
+        configHelper.setLoggerFn(req.app.get('__fnxtlogger__'));
+        const db=configHelper.getDb();
+        var queryResult=db.query(query,{type: db.QueryTypes.SELECT});
+        queryResult.catch(()=>{
+            next(new HttpError(500,'ERR-CON-0021','Unknown error occured'));
+        }).finally(()=>{
+            db.close();
         });
+        if(/[a-zA-Z]{1,}count$/.test(req.params.key)){
+            queryResult.spread(count=>{
+                ioHelper.getSuccessResponse({collections:{response:count,rowcount:1}},isXMLResponse,res)
+            }); 
+        }
+        else{
+            queryResult.then(result=>{
+                ioHelper.getSuccessResponse({collections:{response:result,rowcount:result.length}},isXMLResponse,res)
+            });
+        }
     }
-
-    
+    catch(err){
+        throw new HttpError(500,'FN-XX-xxxx',err.message);
+    }
 };
 module.exports=DownloadHelper;
