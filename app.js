e6fd69001a50bbcaf351cdb7b68d2d58f83972dc
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-04-26 12:47:48 +0530

    dotenv initialisation has been done.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -5,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const HttpError=require('./error/HttpError.js');
 const jsontoxml=require('jsontoxml');
+
 /** Express APP construct **/
 var app = express();
 require('./utils').initExpress(app,{
