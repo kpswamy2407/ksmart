c38d3163218bfdc201c64462c85358dd898e48af
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2019-05-02 20:08:03 +0530

    log func param redefined

--- utils/FnxtLog.js
+++ utils/FnxtLog.js
@@ -5,9 +5,8 @@ function FnxtLog(filename, options){
     if(! (this instanceof FnxtLog)) return new FnxtLog(filename, options);
     RotatingFileStream.call(this,filename,options);
 }
-FnxtLog.prototype.log = function (msg, sep) {
-    if (sep === void 0) { sep = "\n\n"; }
-    this.write(msg + sep);
+FnxtLog.prototype.log = function (msg) {
+    this.write(msg + "\n\n");
     return this;
 };
 util.inherits(FnxtLog,RotatingFileStream);
